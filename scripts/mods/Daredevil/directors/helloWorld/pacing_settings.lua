735c617183e838d4545588e9987c7d62edfc2fba
naviwatch <146077263+naviwatch@users.noreply.github.com>
2024-06-24 08:07:05 +0800

    Delete scripts/mods/Daredevil/directors directory

--- scripts/mods/Daredevil/directors/helloWorld/pacing_settings.lua
+++
@@ -1,5 +0,0 @@
-PacingSettings = PacingSettings or {}
-
-local director_pacing = table.clone(PacingSettings.default)
-
-return director_pacing
--- scripts/mods/Daredevil/directors/helloWorld/packspawn_settings.lua
+++
@@ -1,7 +0,0 @@
-PackSpawningSettings = PackSpawningSettings or {}
-
-local director_packspawn = table.clone(PackSpawningSettings.default)
-
-director_packspawn.area_density_coefficient = 0.115
-
-return director_packspawn
